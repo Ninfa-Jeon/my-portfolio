bbdc1e2279cee5d49e9bdfe9248af19765631b3d
Urvashi Sharma <usharma@falabella.cl>
2023-12-23 21:02:22 +0530

    push code

---
+++ src/index.jsx
@@ -0,0 +1,23 @@
+import "./style.css";
+import ReactDOM from "react-dom/client";
+import { Canvas } from "@react-three/fiber";
+import Experience from "./Experience.jsx";
+import { StrictMode } from "react";
+
+const root = ReactDOM.createRoot(document.querySelector("#root"));
+
+root.render(
+  <StrictMode>
+    <Canvas
+      className="r3f"
+      camera={{
+        fov: 45,
+        near: 0.1,
+        far: 2000,
+        position: [-3, 1.5, 4],
+      }}
+    >
+      <Experience />
+    </Canvas>
+  </StrictMode>
+);
